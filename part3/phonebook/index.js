5dbb7f4b8d2de51e61c8e9fe3c6920eeec21596f
nmikovic <mikovic.nm@gmail.com>
2025-12-16 17:41:26 +0100

    3.13-3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -12,31 +14,13 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
+let persons = []
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({})
+        .then((persons) => {
+            response.json(persons)
+        })
 })
 
 app.get('/info', (request, response) => {
@@ -49,11 +33,11 @@ app.get('/info', (request, response) => {
     `)
 })
 
-app.get('/api/persons/:id', (request,response) => {
+app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
     const person = persons.find(p => p.id == id)
 
-    if(person){
+    if (person) {
         response.json(person)
     } else {
         response.status(404).end()
@@ -70,37 +54,34 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    if(!body.number){
+    if (!body.number) {
         return response.status(400).json({
             error: 'number is missing'
         })
     }
 
-    if(!body.name){
+    if (!body.name) {
         return response.status(400).json({
             error: 'name is missing'
         })
     }
 
-    if(persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())){
-        return  response.status(400).json({
+    if (persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())) {
+        return response.status(400).json({
             error: 'name must be unique'
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id:Math.floor(Math.random() * 1000).toString()
-    }
+        number: body.number
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then((savedPerson) => {
+        response.json(savedPerson)
+    })
 
 })
-
-
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://mikovicnm_db_user:${password}@cluster0.mmctcxh.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://mikovicnm_db_user:${password}@cluster0.mmctcxh.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
