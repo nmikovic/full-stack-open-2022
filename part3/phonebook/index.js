2487c27b8e616de438d58fc54e4bbfbc47df4c56
nmikovic <mikovic.nm@gmail.com>
2025-12-16 19:21:07 +0100

    3.15 - 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,6 +5,19 @@ const Person = require('./models/person')
 
 const app = express()
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({
+            error: 'malformed id'
+        })
+    }
+
+    next(error)
+}
+
+
 app.use(express.static('dist'))
 app.use(express.json())
 
@@ -33,22 +46,28 @@ app.get('/info', (request, response) => {
     `)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(p => p.id == id)
+app.get('/api/persons/:id', (request, response, next) => {
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch((error) => {
+            next(error)
+        })
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -82,6 +101,34 @@ app.post('/api/persons', (request, response) => {
     })
 
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const {name, number} = request.body
+
+    Person.findById(request.params.id)
+        .then(person => {
+            if(!person){
+                return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+
+            return person.save().then(updatedPerson => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({
+        error: 'unknown endpoint'
+    })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
-console.log(process.env.NEMANJA)
 
 console.log('connecting to', url)
 mongoose
